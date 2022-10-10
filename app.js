047e05c806c1d65526a50204ad7d686e75b25627
Allison Valdez <ajsnipes@usfca.edu>
2022-10-10 18:10:29 -0400

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log("works")
 /*
 1: Difference: this function takes in two parameters and returns the difference of the two;
 */
+
 console.log(`1. difference();`);
 
 function difference(numOne, numTwo) {
@@ -15,6 +16,7 @@ console.log(difference(0, 2));
 /*
 2: Product: this function takes in two parameters and returns the product of the two;
 */
+
 console.log(`2. product();`);
 
 function product(x, y) {
@@ -27,6 +29,7 @@ console.log(product(0, 2));
 /*
 3: PrintDay: this function takes in one parameter (a number from 1-7) and returns the day of the week (1 is Sunday, 2 is Monday, 3 is Tuesday etc.). If the number is less than 1 or greater than 7, the function should return undefined;
 */
+
 console.log(`3. printDay();`);
 
 function printDay(num) {
@@ -50,6 +53,7 @@ console.log(printDay(41));
 /*
 4: LastElement: this function takes in one parameter (an array) and returns the last value in the array. It should return undefined if the array is empty.
 */
+
 console.log(`4. lastElement();`);
 
 function lastElement(array) {
@@ -64,6 +68,7 @@ console.log(lastElement([]));
 /*
 5: NumberCompare: this function takes in two parameters (both numbers). If the first is greater than the second, this function returns “First is greater”. If the second number is greater than the first, the function returns “Second is greater”. Otherwise the function returns “Numbers are equal”
 */
+
 console.log(`5. numberCompare();`);
 
 function numberCompare(a, b) {
@@ -85,6 +90,7 @@ console.log(numberCompare(1, 2));
 /*
 6: SingleLetterCount: this function takes in two parameters (two strings). The first parameter should be a word and the second should be a letter. The function returns the number of times that letter appears in the word. The function should be case insensitive (does not matter if the input is lowercase or uppercase). If the letter is not found in the word, the function should return 0.
 */
+
 console.log(`6. singleLetterCount();`);
 
 function singleLetterCount(word, char) {
@@ -105,22 +111,85 @@ console.log(singleLetterCount("Rithm School", "o"));
 /*
 7: MultipleLetterCount: this function takes in one parameter (a string) and returns an object with the keys being the letters and the values being the count of the letter.
 */
+
 console.log(`7. multipleLetterCount();`);
 
 function multipleLetterCount(string) {
-    
+    string = string.toLowerCase();
+    let returnObject = {};
+
+    for (let i = 0; i < string.length; i++) {
+        if (returnObject[string[i]] === undefined) {
+            returnObject[string[i]] = 1;
+        }
+        else {
+            returnObject[string[i]]++;
+        }
+    }
+
+    return returnObject;
+
 }
 
-
-
-
-
-/*
-8: : 
-*/
-console.log(`8. `);
+console.log(multipleLetterCount("hello"));
+console.log(multipleLetterCount("person"));
 
 /*
-9: 
+8: ArrayManipulation: this function should take in at most four parameters (an array, command, location, and value).
+
+If the command is “remove” and the location is “end”, the function should remove the last value in the array and return the value removed. (In this case, the function only needs three arguments.)
+
+If the command is “remove” and the location is “beginning”, the function should remove the first value in the array and return the value removed. (In this case, the function only needs three arguments.)
+
+If the command is “add” and the location is “beginning”, the function should add the value (fourth parameter) to the beginning of the array and return the array.
+
+If the command is “add” and the location is “end”, the function should add the value (fourth parameter) to the end of the array and return the array.
 */
-console.log(`9. `);
+
+console.log(`8. arrayManipulation`);
+
+function arrayManipulation(array, command, location, value) {
+    if (command === "remove") {
+        if (location === "end") {
+            return array.pop();
+        }
+        else {
+            return array.shift();
+        }
+    }
+    else if (command === "add") {
+        if (location === "end") {
+            array.push(value);
+            return array;
+        }
+        else {
+            array.unshift(value);
+            return array;
+        }
+    }
+}
+
+console.log(arrayManipulation([1, 2, 3], "remove", "end"));
+console.log(arrayManipulation([1, 2, 3], "remove", "beginning"));
+console.log(arrayManipulation([1, 2, 3], "add", "beginning", 20));
+console.log(arrayManipulation([1, 2, 3], "add", "end", 30));
+
+/*
+9: IsPalindrome: A Palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This function should take in one parameter and returns true or false if it is a palindrome. As a bonus, allow your function to ignore whitespace and capitalization so that isPalindrome(‘a man a plan a canal Panama’); returns true
+*/
+
+console.log(`9. isPalindrome`);
+
+function isPalindrome(chars) {
+    for (let i = 0; i < chars.length / 2; i++) {
+        if (chars[i].toLowerCase() !== chars[chars.length - 1 - i].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome('testing'));
+console.log(isPalindrome('tacocat'));
+console.log(isPalindrome('hannah'));
+console.log(isPalindrome('robert'));
